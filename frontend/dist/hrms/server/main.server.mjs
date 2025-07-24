49296499a3d6db15eac515f69064aa5f50a0c55b
Ubuntu <ubuntu@ip-172-31-2-156.ap-south-1.compute.internal>
2025-07-24 09:58:37 +0000

    Force add frontend/dist folder

---
+++ frontend/dist/hrms/server/main.server.mjs
@@ -0,0 +1,2 @@
+import './polyfills.server.mjs';
+import{e as a}from"./chunk-D4ZDTWR4.mjs";import"./chunk-2M4DUVBH.mjs";import"./chunk-U4KSSDP6.mjs";import"./chunk-FME56UVT.mjs";export{a as default};
---
+++ frontend/dist/hrms/server/render-utils.server.mjs
@@ -0,0 +1,2 @@
+import './polyfills.server.mjs';
+import{m as o,n as r,o as n}from"./chunk-2M4DUVBH.mjs";import{Wb as e}from"./chunk-U4KSSDP6.mjs";import"./chunk-FME56UVT.mjs";export{n as renderApplication,r as renderModule,e as \u0275Console,o as \u0275SERVER_CONTEXT};
